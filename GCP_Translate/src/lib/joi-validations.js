49df8aa288dba05408fd5fbf302e6175e4467157
G <ramkumar.a.g@capgemini.com>
2022-03-09 19:12:20 +0530

    version added

--- GCP_Translate/src/lib/joi-validations.js
+++ GCP_Translate/src/lib/joi-validations.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const nameSchema = Joi.string()
   .min(1)
-  .max(30)
-  .label('Character should be between 1 and 30 characters.');
+  .max(60)
+  .label('Character should be between 1 and 60 characters.');
 
 module.exports = {nameSchema};
